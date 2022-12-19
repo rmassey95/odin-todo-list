df9274bb6698c277ba351832de5280b149613e62
rmassey95 <rmassey.95@gmail.com>
2022-12-19 10:00:36 -0500

    commit

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -1,35 +1,42 @@
-const Task = (i, title, desc, prio, proj, dueDate) => {
+import uniqid from "uniqid";
 
-  return {i, title, desc, prio, proj, dueDate};
+const Task = (i, title, desc, prio, proj, dueDate) => {
+  return { i, title, desc, prio, proj, dueDate };
 };
 
 const updateTask = (() => {
-
   const updateTitle = (task, newTitle) => {
     task.title = newTitle;
-  } 
+  };
 
   const updateDesc = (task, newDesc) => {
     task.desc = newDesc;
-  }
+  };
 
   const updatePrio = (task, newPrio) => {
     task.prio = newPrio;
-  }
+  };
 
   const updateProj = (task, newProj) => {
     task.proj = newProj;
-  }
+  };
 
   const updateDate = (task, newDate) => {
     task.dueDate = newDate;
-  }
+  };
 
   const updateIndex = (task, newI) => {
     task.i = newI;
-  }
+  };
 
-  return {updateTitle, updateDesc, updatePrio, updateIndex, updateProj, updateDate};
+  return {
+    updateTitle,
+    updateDesc,
+    updatePrio,
+    updateIndex,
+    updateProj,
+    updateDate,
+  };
 })();
 
 const taskList = (() => {
@@ -42,19 +49,19 @@ const taskList = (() => {
   const taskLength = () => tasks.length;
 
   const removeTask = (index) => {
-    const arrIndex = getTaskList().findIndex(x => x.i === index);
-    tasks.splice(arrIndex,1);
-  }
+    const arrIndex = getTaskList().findIndex((x) => x.i === index);
+    tasks.splice(arrIndex, 1);
+  };
 
   const saveTasks = () => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
 
   const pageLoadTasks = () => {
-    const results = JSON.parse(localStorage.getItem('tasks'));
+    const results = JSON.parse(localStorage.getItem("tasks"));
 
     tasks = results;
-  }
+  };
 
   const getTaskList = () => tasks;
 
@@ -62,16 +69,15 @@ const taskList = (() => {
 })();
 
 const displayForm = () => {
-  const idInput = document.querySelector('#index');
-  const formDiv = document.querySelector('.form-container');
-  formDiv.style.display = 'block';
+  const idInput = document.querySelector("#index");
+  const formDiv = document.querySelector(".form-container");
+  formDiv.style.display = "block";
 
-  if (idInput.value === '') {
+  if (idInput.value === "") {
     //
   } else {
-    document.querySelector('.delete').style.display = 'inline-block';
+    document.querySelector(".delete").style.display = "inline-block";
   }
-
 };
 
-export {displayForm, Task, taskList, updateTask};
+export { displayForm, Task, taskList, updateTask };
